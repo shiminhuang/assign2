69f0342ac489835238599d8fbd8d9c15ec00f948
shimin <huangshimin.irene@gmail.com>
2018-05-11 15:36:36 +0200

    topology changed

--- topology/MyTopo.py
+++ topology/MyTopo.py
@@ -17,8 +17,8 @@ class MyTopo(Topo):
 	#add hosts and switches
 	#public zone
 		sw1= self.addSwitch('s1')
-		h1=self.addHost('h1',ip='100.0.0.11/24')
-		h2=self.addHost('h2',ip='100.0.0.12/24')
+		h1=self.addHost('h1',ip='100.0.0.10/24',mac='00:00:00:00:00:10', defaultRoute='via 100.0.0.1')
+		h2=self.addHost('h2',ip='100.0.0.11/24', mac='00:00:00:00:00:11', defaultRoute='via 100.0.0.1')
 		fw1=self.addSwitch('s2')
 
 	#demilitarized zone
@@ -26,23 +26,24 @@ class MyTopo(Topo):
 		lb1=self.addSwitch('s4')
 		sw3=self.addSwitch('s5')
 		ids=self.addSwitch('s6')
-		insp=self.addHost('h11', ip='100.0.0.30/24')
+		insp=self.addHost('h11', ip='100.0.0.30/24', mac='00:00:00:00:00:30')
 		lb2=self.addSwitch('s7')
 		sw4=self.addSwitch('s8')
-		ds1=self.addHost('h5',ip='100.0.0.20/24')
-		ds2=self.addHost('h6',ip='100.0.0.21/24')
-		ds3=self.addHost('h7',ip='100.0.0.22/24')
-		ws1=self.addHost('h8',ip='100.0.0.40/24')
-		ws2=self.addHost('h9',ip='100.0.0.41/24')
-		ws3=self.addHost('h10',ip='100.0.0.42/24')
+		ds1=self.addHost('h5',ip='100.0.0.20/24', mac='00:00:00:00:00:20')
+		ds2=self.addHost('h6',ip='100.0.0.21/24', mac='00:00:00:00:00:21')
+		ds3=self.addHost('h7',ip='100.0.0.22/24', mac='00:00:00:00:00:22')
+		ws1=self.addHost('h8',ip='100.0.0.40/24', mac='00:00:00:00:00:40')
+		ws2=self.addHost('h9',ip='100.0.0.41/24', mac='00:00:00:00:00:41')
+		ws3=self.addHost('h10',ip='100.0.0.42/24', mac='00:00:00:00:00:42')
 
 	# private zone
 		fw2=self.addSwitch('s9')
 		napt=self.addSwitch('s10')
 		sw5=self.addSwitch('s11')
-		h3=self.addHost('h3',ip='100.0.0.51/24')
-		h4=self.addHost('h4',ip='100.0.0.52/24')
+		h3=self.addHost('h3',ip='10.0.0.50/24',mac='00:00:00:00:00:50', defaultRoute='via 10.0.0.1')
+		h4=self.addHost('h4',ip='10.0.0.51/24', mac='00:00:00:00:00:51', defaultRoute='via 10.0.0.1')
 
+		
 	# public links
 		self.addLink(h1,sw1)
 		self.addLink(h2,sw1)
@@ -70,23 +71,6 @@ class MyTopo(Topo):
 	# private zone
 		self.addLink(sw5,h3)
 		self.addLink(sw5,h4)
-#def Phase1():
-	#c0 = RemoteController('c0', ip='127.0.0.1', port=6633)
-   	#net = Mininet(topo=MyTopo(), controller=c0)
-   	#net.start()
-
-   	#ds1 = net.get('h5')
-    	#ws1 = net.get('h8')
-
-   	#ds1.cmd('python dns.py &')
-    	#ws1.cmd('python -m SimpleHTTPServer 80 &')
-
-    #CLI(net)
-    	#test(net)
-    	#net.stop()
-	
-#if __name__ == '__main__':
-    	#setLogLevel('info')
-   	#Phase1()
+		
 
 topos={'mytopo':( lambda: MyTopo())}
